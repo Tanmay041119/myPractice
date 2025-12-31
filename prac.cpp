25e922acd276df9f7afb0e0acb24c2dd3bdb8929
Tanmay041119 <tanmaykhandelwal0411@gmail.com>
2025-12-31 23:54:10 +0530

    cloned dev

--- prac.cpp
+++ prac.cpp
@@ -4,5 +4,6 @@ int main(){
     cout<<"Hello world"<<endl;
     cout<<"How are you"<<endl;
     cout<<"I have written this in developer"<<endl;
+    cout<<"I have written this in cloned file develpoer"<<endl;
     return 0;
 }
